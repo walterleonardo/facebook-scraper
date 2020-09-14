28455612a951e0d9c20877c217aacf9a6481a38a
Walter Leonardo López Pascual <walterleonardo@gmail.com>
2020-09-14 10:20:06 +0200

    update test file, issue with login

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from facebook_scraper import get_posts
 
-for post in get_posts("nintendo", pages=2, comments = True, credentials=['username','password']):
+for post in get_posts("nintendo", pages=1, comments = True, credentials=['username','password']):
     print(post)
